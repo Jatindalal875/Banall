a6aff605e24a0cff2c92a5763734f895e1410654
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2024-08-05 00:26:34 +0530

    Update var.py

--- var.py
+++ var.py
@@ -6,4 +6,4 @@ class Var:
     API_ID = config("API_ID", "12834603")
     API_HASH = config("API_HASH", "84a5daf7ac334a70b3fbd180616a76c6")
     BOT_TOKEN = config("BOT_TOKEN", "6233213793:AAHh5Nkoi8MNaodGInmONsQONCeatV6zZtc")
-    SUDO = list(map(int, getenv("SUDO", "5615344987, 6699370215").split()))
+    SUDO = list(map(int, getenv("SUDO", "5615344987,6699370215").split()))
